5d0b596c05899310b442b034a430914e5e697240
pablitosko <78922639+pablitosko@users.noreply.github.com>
2021-10-31 15:35:52 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencia(),
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(200.00, 2000)),
+        ItemTransferencia(Transferencia(300.00, 3000)),
+        ItemTransferencia(Transferencia(400.00, 4000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
